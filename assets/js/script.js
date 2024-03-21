fe8197d28dec4fa2f7014857677c7b07ed49c3c9
Helena Bjornberg <helena.bjornberg@hotmail.com>
2024-03-21 16:26:01 +0000

    Add divison game

--- assets/js/script.js
+++ assets/js/script.js
@@ -51,6 +51,8 @@ function runGame(gameType) {
         displayMultiplyQuestion(num1, num2);
     } else if (gameType === "subtract") {
         displaySubtractQuestion(num1, num2);
+    } else if (gameType === "division") { 
+        displayDivisionQuestion(num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -98,6 +100,14 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, "multiply"];
     } else if (operator === "-") {
         return [operand1 - operand2, "subtract"];
+    } else if (operator === "/") {
+        if (operand2 === 0) {
+
+            throw "Division by zero error"; 
+          
+        }
+        return [operand1 / operand2, "division"];
+
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -143,5 +153,13 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "x";
+
+}
+
+function displayDivisionQuestion(operand1, operand2) {
+
+    document.getElementById("operand1").textContent = operand1 * operand2; 
+    document.getElementById("operand2").textContent = operand2; 
+    document.getElementById("operator").textContent = "/";
 
 }
